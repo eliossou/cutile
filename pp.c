0799754b69c0fc2c8923bb0e104031cffeffc857
eliossou <eliossou@eliossou.dev>
2026-08-17 19:34:58 +0200

    Add cut_va_args_for_each.

--- pp.c
+++ pp.c
@@ -9,7 +9,39 @@
     #define cut_va_args_count__( _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, \
                                  N, ...) N
 
+    // Applies a macro to each argument passed to (...), expanding to:
+    //     apply(arg1) apply(arg2) ...
+    // Requires at least one argument after the apply macro. Supports up to 16 arguments.
+    #define cut_va_args_for_each(apply, ...) \
+        cut_va_args_for_each__(apply, __VA_ARGS__, \
+            cut_va_args_for_each_16, cut_va_args_for_each_15, cut_va_args_for_each_14, cut_va_args_for_each_13, \
+            cut_va_args_for_each_12, cut_va_args_for_each_11, cut_va_args_for_each_10, cut_va_args_for_each_9, \
+            cut_va_args_for_each_8, cut_va_args_for_each_7, cut_va_args_for_each_6, cut_va_args_for_each_5, \
+            cut_va_args_for_each_4, cut_va_args_for_each_3, cut_va_args_for_each_2, cut_va_args_for_each_1) \
+        (apply, __VA_ARGS__)
+
+    #define cut_va_args_for_each__( _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, \
+                                    apply, ...) apply
+
+    #define cut_va_args_for_each_1(apply, x, ...) apply(x)
+    #define cut_va_args_for_each_2(apply, x, ...) apply(x) cut_va_args_for_each_1(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_3(apply, x, ...) apply(x) cut_va_args_for_each_2(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_4(apply, x, ...) apply(x) cut_va_args_for_each_3(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_5(apply, x, ...) apply(x) cut_va_args_for_each_4(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_6(apply, x, ...) apply(x) cut_va_args_for_each_5(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_7(apply, x, ...) apply(x) cut_va_args_for_each_6(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_8(apply, x, ...) apply(x) cut_va_args_for_each_7(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_9(apply, x, ...) apply(x) cut_va_args_for_each_8(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_10(apply, x, ...) apply(x) cut_va_args_for_each_9(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_11(apply, x, ...) apply(x) cut_va_args_for_each_10(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_12(apply, x, ...) apply(x) cut_va_args_for_each_11(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_13(apply, x, ...) apply(x) cut_va_args_for_each_12(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_14(apply, x, ...) apply(x) cut_va_args_for_each_13(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_15(apply, x, ...) apply(x) cut_va_args_for_each_14(apply, __VA_ARGS__)
+    #define cut_va_args_for_each_16(apply, x, ...) apply(x) cut_va_args_for_each_15(apply, __VA_ARGS__)
+
     #if defined(CUT_PP_SHORT_NAMES) || defined(CUT_SHORT_NAMES)
         #define va_args_count cut_va_args_count
+        #define va_args_for_each cut_va_args_for_each
     #endif
 #endif
--- tests/pp_tests.c
+++ tests/pp_tests.c
@@ -2,12 +2,15 @@
 
 #define count_fwd(...) cut_va_args_count(__VA_ARGS__)
 
-enum { cut_pp_test_n3 = cut_va_args_count(1, 2, 3) };
+#define strfy(x) #x
+#define add(x) + (x)
+#define comma(x) x,
+#define cut_enum_field(x) cut_pp_e_##x,
+
+enum { cut_va_args_for_each(cut_enum_field, red, green, blue) cut_pp_e_count };
 
 void run_pp_tests()
 {
-    test(cut_pp_test_n3 == 3);
-
     test(cut_va_args_count(0) == 1);
     test(count_fwd(0) == 1);
 
@@ -30,4 +33,25 @@ void run_pp_tests()
     // Result is usable wherever an integer constant expression is expected.
     int arr[cut_va_args_count(a, b, c, d)];
     test(cut_array_size(arr) == 4);
+
+    // For each
+    test(cut_pp_e_count == 3);
+    test(cut_pp_e_red == 0);
+    test(cut_pp_e_green == 1);
+    test(cut_pp_e_blue == 2);
+
+    test(str0_eq(cut_va_args_for_each(strfy, a, b, c), "abc"));
+    test(str0_eq(cut_va_args_for_each(strfy, 0, 1, 2, 3), "0123"));
+    test(str0_eq(cut_va_args_for_each(strfy, a, b + c, (x)(y), "str"), "ab + c(x)(y)\"str\""));
+
+    // For each is usable wherever a list of tokens is expected.
+    int vals[] = { cut_va_args_for_each(comma, 10, 20, 30, 40) };
+    test(cut_array_size(vals) == 4);
+    test(vals[0] == 10 && vals[1] == 20 && vals[2] == 30 && vals[3] == 40);
+
+    int total = (0 cut_va_args_for_each(add, 1, 2, 3));
+    test(total == 6);
+
+    int total16 = (0 cut_va_args_for_each(add, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16));
+    test(total16 == 136);
 }
